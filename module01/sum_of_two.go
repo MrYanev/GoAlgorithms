a9bd2b8752cd24d7eb6064f3dd18b0c44f3be0bd
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-18 15:56:29 +0100

    Update sum_of_two.go

--- module01/sum_of_two.go
+++ module01/sum_of_two.go
@@ -1,15 +1,12 @@
 package module01
 
 func SumOfTwo(numList []int, sum int) (int, int) {
-	var ans1, ans2 int
-
 	for idx1, i := range numList {
 		for idx2, j := range numList[idx1:] {
 			if i+j == sum {
-				ans1 = idx1
-				ans2 = idx2
+				return idx1, idx2
 			}
 		}
 	}
-	return ans1, ans2
+	return -1, -1
 }
