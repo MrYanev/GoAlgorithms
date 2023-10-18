f97a2f61558ad5bf351f2923b2251a17597cda85
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-18 13:21:09 +0100

    Update base_to_dec.go

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,6 +1,14 @@
 package module01
 
-func BaseToDec(value string, base int) int {
+import (
+	"math"
+)
 
-	return 0
+func BaseToDec(value string, base int) int {
+	var ans int
+	for idx, val := range value {
+		res := math.Pow(float64(base), float64((len(value) - idx)))
+		ans += int(val) * int(res)
+	}
+	return ans
 }
