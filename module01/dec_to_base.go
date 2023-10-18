f2250c882d550dfb6b718de3c6d995fc2df9e417
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-18 08:48:03 +0100

    Update dec_to_base.go

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,6 +1,13 @@
 package module01
 
+import "fmt"
+
 func DecToBase(dec, base int) string {
 	var ans string
+	for dec != 0 {
+		tmp := dec % base
+		ans = fmt.Sprintf("%X", tmp) + ans
+		dec = dec / base
+	}
 	return ans
 }
