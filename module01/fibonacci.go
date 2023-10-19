023d15f9fb35cba742d3ff9fd8af310f06d0d151
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-19 09:14:37 +0100

    Update fibonacci.go

--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -1,11 +1,9 @@
 package module01
 
 func Fibonacci(num int) int {
-	var ans int
-	for i := 0; i >= num; i++ {
-		if i == 0 || i == 1 {
-			ans += 1
-		}
+	if num <= 1 {
+		return 1
+	} else {
+		return Fibonacci(num-2) + Fibonacci(num-1)
 	}
-	return ans
 }
