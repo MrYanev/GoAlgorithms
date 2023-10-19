4df81996670f46317741b1ca6227390db8bd9320
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-19 08:45:18 +0100

    Update factor.go

--- module01/factor.go
+++ module01/factor.go
@@ -2,5 +2,11 @@ package module01
 
 func Factor(primes []int, number int) []int {
 	var ans []int
+	for _, num := range primes {
+		if number%num == 0 {
+			number = number / num
+			ans = append(ans, num)
+		}
+	}
 	return ans
 }
