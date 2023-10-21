213d61ea967900c43c7fb2bdf9bc82b93e960945
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-21 08:13:12 +0100

    Update bubble_sort.go

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -2,5 +2,16 @@ package module02
 
 func BubbleSort(list []int) []int {
 	var sorted []int
+	var srt = false
+	for !srt {
+		for i := 0; i <= len(list)-1; i++ {
+			if list[i] > list[i+1] {
+				tmp := list[i+1]
+				list[i+1] = list[i]
+				list[i] = tmp
+			}
+		}
+	}
+
 	return sorted
 }
