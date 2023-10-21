f84d04920b71a2e92ea21073dd35f9d035446bf8
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-21 12:42:58 +0100

    Update insertion_sort.go

--- module02/insertion_sort.go
+++ module02/insertion_sort.go
@@ -2,5 +2,11 @@ package module02
 
 func InsertionSort(list []int) []int {
 	var sorted []int
+	for _, i := range list {
+		if len(sorted) == 0 {
+			sorted = append(sorted, i)
+		}
+
+	}
 	return sorted
 }
